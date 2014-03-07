4e29a870f9eb993bef4f5240869e27a94ede6111
Jony <zj86@live.cn>
2014-03-07 18:27:41 +0800

    Update js

--- tpl.debug.js
+++ tpl.debug.js
@@ -1,12 +1,14 @@
-/*! tpl.js 0.2.0, github.com/niceue/tpl.js */
+/*! tpl.js 0.3.0, github.com/niceue/tpl.js */
 
 /* 类似于PHP的嵌入方式, 可以嵌入js语句
    模板语法：
-       嵌入传入的变量: <%=xxx%> , 注意，xxx变量不能为javascript关键字
-       嵌入任意js语句：<% if(xxx){ %> foo <%}else{%> bar <%}%>
+       嵌入传入的变量: <#=xxx#> , 注意，xxx变量不能为javascript关键字
+       嵌入任意js语句：<#if(xxx){#> foo <#}else{#> bar <#}#>
    调用：
-       var html = tpl('#tpl_id', data);
-       console.log(html);
+        方式一(直接输出结果)：tpl(template, data)
+        方式二(预编译，二次调用传入data)：tpl(template)(data)
+
+        其中，如果template变量传入#id，会自动获取innerHTML
  */
 (function (root, factory) {
     //AMD and CMD (RequireJS, OzJS, curl, SeaJS ...)
@@ -18,8 +20,8 @@
     function Compiler(html) {
         html = html || '';
         if (/^#\w+$/.test(html)) html = document.getElementById(html.substring(1)).innerHTML;
-        var begin = '<%',
-            end = '%>',
+        var begin = '<#',
+            end = '#>',
             trim = function(str) {
                 return str.trim ? str.trim() : str.replace(/^\s*|\s*$/g, '');
             },
@@ -40,7 +42,7 @@
             tmp = trim(html.substring(b+blen, e));
             if( tmp.indexOf('=') === 0 ) { //模板变量
                 tmp = tmp.substring(1);
-                str += "typeof(" + tmp + ")!='undefined'&&(__+=" + tmp + ");";
+                str += "typeof(" + tmp + ")!=='undefined'&&(__+=" + tmp + ");";
             } else { //js代码
                 str += tmp;
             }
@@ -53,6 +55,6 @@
 
     return function(html, data) {
         var me = new Compiler(html);
-        return data ? me.render(data) : me;
+        return data ? me.render(data) : me.render;
     };
 }));
